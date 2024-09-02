ca9e61dbf40e2a23354a3b90100b04542bc8d935
Steven Schluchter <steve@Stevens-MacBook-Pro.local>
2024-09-02 15:21:11 -0700

    first commit

--- Silly_Scripts_Python/wordle.py
+++ Silly_Scripts_Python/wordle.py
@@ -22,7 +22,7 @@ def word_score(input_word):
 
     for letter in input_word_list:
         
-        score += 25 - letter_frequency_array.index(letter)
+        score += 26 - letter_frequency_array.index(letter)
 
     return score
 
